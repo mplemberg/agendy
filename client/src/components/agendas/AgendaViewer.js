5dde775584a3fd2b894c0ec8c39dfec84922cb0a
mlemb <mplemberg@gmail.com>
2019-12-16 20:44:37 -0800

    matching up view and draft styles

--- client/src/components/agendas/AgendaViewer.js
+++ client/src/components/agendas/AgendaViewer.js
@@ -17,27 +17,51 @@ const AgendaViewer = ({ match }) => {
   }, []);
 
   if (loading) return <Spinner />;
-
+  const leftColumnSpacer = (
+    <div className='col-auto pr-0'>
+      {
+        //This is needed to line all the text up and have the drag icon to the left
+      }
+      <div
+        style={{
+          width: "24px"
+        }}
+      ></div>
+    </div>
+  );
   const { name, agendaLines } = agenda;
   return (
     <main>
-      <div className='my-3'>
-        <div className='row'>
-          <div className='col-auto'>
-            <div className='h1'>{name}</div>
-          </div>
-        </div>
-        {agenda.publishedDate && (
-          <div className='row'>
-            <div className='col-auto'>
-              <div className='small font-italic'>
-                Last Updated: {agenda.publishedDate}
+      <div className='row'>
+        <div className='col'>
+          <div className='row my-3'>
+            {leftColumnSpacer}
+            <div className='col pl-1'>
+              <div className='row'>
+                <div className='col-auto'>
+                  <div className='h1'>{name}</div>
+                </div>
               </div>
+              {agenda.publishedDate && (
+                <div className='row'>
+                  <div className='col-auto'>
+                    <div className='small font-italic'>
+                      Last Updated: {agenda.publishedDate}
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-        )}
+          {agendaLines && (
+            <div className='row'>
+              <div className='col'>
+                <OutlineViewer />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      {agendaLines && <OutlineViewer />}
     </main>
   );
 };
--- client/src/components/agendas/DisplayOutlineItem.js
+++ client/src/components/agendas/DisplayOutlineItem.js
@@ -8,9 +8,28 @@ const DisplayOutlineItem = ({ item }) => {
     margin = "ml-5";
   }
 
+  const leftColumnSpacer = (
+    <div className='col-auto pr-0'>
+      {
+        //This is needed to line all the text up and have the drag icon to the left
+      }
+      <div
+        style={{
+          width: "24px"
+        }}
+      ></div>
+    </div>
+  );
   return (
-    <div className={`${margin}`}>
-      <BulletIcon indent={item.indent} /> {item.text}{" "}
+    <div className='row'>
+      {leftColumnSpacer}
+      <div className='col pl-1'>
+        <div className={`row ${margin}`}>
+          <div className='col-auto'>
+            <BulletIcon indent={item.indent} /> {item.text}{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
